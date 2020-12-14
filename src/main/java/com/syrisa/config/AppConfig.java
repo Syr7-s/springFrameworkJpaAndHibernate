2b9eb1aa85c15fb5225399f9479ae4bb5fdd6ece
Syr7-s <isasayar7@github.com>
2020-12-14 21:29:05 +0300

    AppConfig edited.

--- src/main/java/com/syrisa/config/AppConfig.java
+++ src/main/java/com/syrisa/config/AppConfig.java
@@ -1,5 +1,7 @@
 package com.syrisa.config;
 
+import com.syrisa.DataAccess.Abstract.ICityDao;
+import com.syrisa.DataAccess.Concrete.HibernateCityDao;
 import com.syrisa.Entities.City;
 import org.hibernate.Hibernate;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,12 +12,6 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.core.env.Environment;
 import javax.persistence.EntityManager;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.*;
-import org.springframework.core.env.Environment;
-import org.springframework.orm.hibernate5.HibernateTransactionManager;
-import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 @Configuration
 @PropertySource(value = "classpath:hibernate.properties",encoding = "UTF-8")
 @ComponentScan("com.syrisa")
@@ -25,6 +21,10 @@ public class AppConfig {
     @Bean
     public EntityManager entityCityDao(){
         return (EntityManager)new LocalSessionFactoryBean();
+    }
+    @Bean
+    public ICityDao cityDao(){
+        return new HibernateCityDao(entityCityDao());
     }
 
 }
