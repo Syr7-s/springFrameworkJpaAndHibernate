dc2835c0b1d77858b0a4ac9ed5c64e467ee9b0da
Syr7-s <isasayar7@github.com>
2020-12-14 21:56:59 +0300

    HibernateCityDao edited.

--- src/main/java/com/syrisa/DataAccess/Concrete/HibernateCityDao.java
+++ src/main/java/com/syrisa/DataAccess/Concrete/HibernateCityDao.java
@@ -3,6 +3,7 @@ package com.syrisa.DataAccess.Concrete;
 import com.syrisa.DataAccess.Abstract.ICityDao;
 import com.syrisa.Entities.City;
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
@@ -12,39 +13,39 @@ import javax.persistence.Entity;
 import javax.persistence.EntityManager;
 import java.util.List;
 @Repository
-
 public class HibernateCityDao implements ICityDao<City> {
-   private EntityManager entityManager;
-   @Autowired
-   public HibernateCityDao(EntityManager entityManager){
-       this.entityManager=entityManager;
-   }
+    @Autowired
+    private SessionFactory sessionFactory;
+    public Session getCurrentSession(){
+        return sessionFactory.getCurrentSession();
+    }
     @Override
     @Transactional
     public List<City> getAll() {
-        Session session=entityManager.unwrap(Session.class);
-        List<City> cities=session.createQuery("from City",City.class).getResultList();
+        List<City> cities=getCurrentSession().createQuery("from City ",City.class).getResultList();
         return cities;
 
     }
 
     @Override
     public City getById(int id) {
-        return null;
+        City city=getCurrentSession().get(City.class,id);
+        return city;
     }
 
     @Override
     public void add(City city) {
-
+        getCurrentSession().saveOrUpdate(city);
     }
 
     @Override
     public void update(City city) {
-
+        getCurrentSession().saveOrUpdate(city);
     }
 
     @Override
     public void delete(City city) {
-
+       City cityToDelete=getCurrentSession().get(City.class,city.getId());
+       getCurrentSession().delete(cityToDelete);
     }
 }
