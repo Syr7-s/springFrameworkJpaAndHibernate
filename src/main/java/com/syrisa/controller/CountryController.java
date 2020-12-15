ebb8dbb40f04b7db71b0773941059d6033c8c549
Syr7-s <isasayar7@github.com>
2020-12-15 17:30:36 +0300

    CountryController was edited.

--- src/main/java/com/syrisa/controller/CountryController.java
+++ src/main/java/com/syrisa/controller/CountryController.java
@@ -6,10 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/country")
 public class CountryController {
 
@@ -32,6 +33,6 @@ public class CountryController {
                     country.getCountryGovernmentForm()+" "+
                     country.getCountryHeadOfState());
         }
-        return "Tum ulkeler getirildi.";
+        return "Tum ulkeler getirildi";
     }
 }
