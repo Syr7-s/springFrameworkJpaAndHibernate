07c6b0863933d2ceff8467c49d0aae2e8aac4b34
Syr7-s <isasayar7@github.com>
2020-12-15 17:44:05 +0300

    CountryController was edited.

--- src/main/java/com/syrisa/controller/CountryController.java
+++ src/main/java/com/syrisa/controller/CountryController.java
@@ -3,8 +3,8 @@ package com.syrisa.controller;
 import com.syrisa.Entities.Country;
 import com.syrisa.Service.Abstract.ICountryService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,5 +34,20 @@ public class CountryController {
                     country.getCountryHeadOfState());
         }
         return "Tum country getirildi";
+    }
+    //This request may fail.Because Have overloading in CountryDao.So The machine do not know
+    //which run.
+    @GetMapping("/country/{countryCode}")
+    public String getCountryCode(@PathVariable String countryCode){
+        Country country=(Country) countryService.getByCode(countryCode);
+        System.out.println(country.getCountryCode()+" "+
+                country.getCountryName()+" "+
+                country.getCoutryContinent()+" "+
+                country.getCountryRegion()+" "+
+                country.getCountryPopulation()+" "+
+                country.getCountryLocalName()+" "+
+                country.getCountryGovernmentForm()+" "+
+                country.getCountryHeadOfState());
+        return "Yapilan istek gerceklesdi.";
     }
 }
