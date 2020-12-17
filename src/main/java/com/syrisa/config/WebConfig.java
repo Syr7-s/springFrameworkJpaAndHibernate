d958ea66f63b5bf9c31a14425f70b5419b38efa3
Syr7-s <isasayar7@github.com>
2020-12-17 12:29:40 +0300

    WebConfig was edited.

--- src/main/java/com/syrisa/config/WebConfig.java
+++ src/main/java/com/syrisa/config/WebConfig.java
@@ -5,8 +5,14 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import java.util.Locale;
 
 @Configuration
 @EnableWebMvc
@@ -23,5 +29,31 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
         configurer.enable();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeInterceptor()).addPathPatterns("/*");
+    }
+
+    @Bean
+    public CookieLocaleResolver localeResolver(){
+        CookieLocaleResolver cookieLocaleResolver=new CookieLocaleResolver();
+        cookieLocaleResolver.setDefaultLocale(Locale.forLanguageTag("tr-TR"));
+        cookieLocaleResolver.setCookieName("syrisa-locale-cookie");
+        cookieLocaleResolver.setCookieMaxAge(3600);
+        return cookieLocaleResolver;
+    }
+    @Bean
+    public SessionLocaleResolver sessionLocaleResolver(){
+        SessionLocaleResolver sessionLocaleResolver=new SessionLocaleResolver();
+        sessionLocaleResolver.setDefaultLocale(Locale.forLanguageTag("tr-TR"));
+        return sessionLocaleResolver;
+    }
+    @Bean
+    public LocaleChangeInterceptor localeInterceptor(){
+        LocaleChangeInterceptor interceptor=new LocaleChangeInterceptor();
+        interceptor.setParamName("lang");
+        return interceptor;
     }
 }
