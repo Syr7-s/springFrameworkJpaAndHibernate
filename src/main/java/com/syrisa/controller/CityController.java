bbd38552f7fb6ea4c5e82d355d997221ad90f21b
Syr7-s <isasayar7@github.com>
2020-12-18 21:19:54 +0300

    CityController was edited.

--- src/main/java/com/syrisa/controller/CityController.java
+++ src/main/java/com/syrisa/controller/CityController.java
@@ -4,6 +4,7 @@ import com.syrisa.Entities.City;
 import com.syrisa.Service.Abstract.ICityService;
 import org.apache.tiles.request.Request;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
