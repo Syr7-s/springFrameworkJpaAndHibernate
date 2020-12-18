2c3d7e131c0cd5a32702de0ab86dacaf4eb210c2
Syr7-s <isasayar7@github.com>
2020-12-18 20:00:16 +0300

    home.jsp and index.jsp were edited,web.xml was edited.HomeController was edited.

--- src/main/java/com/syrisa/controller/HomeController.java
+++ src/main/java/com/syrisa/controller/HomeController.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping("/home")
     public String getHomePage(){
-        return "home";
+        return "views/home";
     }
 }
