8ff75d2348d88f10e4f8e1be6901fc66f9804781
Syr7-s <isasayar7@github.com>
2020-12-18 21:36:04 +0300

    HomeController was edited.

--- src/main/java/com/syrisa/controller/HomeController.java
+++ src/main/java/com/syrisa/controller/HomeController.java
@@ -3,12 +3,14 @@ package com.syrisa.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 @RequestMapping("/home")
 public class HomeController {
     @GetMapping("/home")
+    @ResponseBody
     public String getHomePage(){
         return "home";
     }
