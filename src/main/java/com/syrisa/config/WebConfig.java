f84a1457aeebae79ac8280b97587bedcd326dca1
Syr7-s <isasayar7@github.com>
2020-12-18 21:23:27 +0300

    home.jsp,index.jsp,web.xml and HomeController were edited.

--- src/main/java/com/syrisa/config/WebConfig.java
+++ src/main/java/com/syrisa/config/WebConfig.java
@@ -3,6 +3,9 @@ package com.syrisa.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -12,16 +15,19 @@ import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan(basePackages = "com.syrisa")
+@ComponentScan(basePackages = {"com.syrisa"})
 public class WebConfig implements WebMvcConfigurer {
     @Bean
     public InternalResourceViewResolver viewResolver() {
         InternalResourceViewResolver internalResourceViewResolver = new InternalResourceViewResolver();
-        internalResourceViewResolver.setPrefix("/");
+        internalResourceViewResolver.setPrefix("/WEB-INF/views/");
         internalResourceViewResolver.setSuffix(".jsp");
         return internalResourceViewResolver;
     }
@@ -30,7 +36,24 @@ public class WebConfig implements WebMvcConfigurer {
     public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
         configurer.enable();
     }
+    /*
+     *Response Karakter Kodlaması İçin
+     * HTTP Response'un da Http Requestte olduğu  gibi UTF-8 karakter kodlamasına göre kodlanması gerekmektedir.
+     */
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        StringHttpMessageConverter stringConverter = new StringHttpMessageConverter();
 
+        List<MediaType> mediaTypeList = new ArrayList<>();
+        mediaTypeList.add(new MediaType("text", "plain", Charset.forName("UTF-8")));
+        mediaTypeList.add(new MediaType("text", "plain", Charset.forName("UTF-8")));
+        mediaTypeList.add(new MediaType("application", "json", Charset.forName("UTF-8")));
+        mediaTypeList.add(new MediaType("text", "javascript", Charset.forName("UTF-8")));
+
+        stringConverter.setSupportedMediaTypes(mediaTypeList);
+        converters.add(stringConverter);
+
+    }
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(localeInterceptor()).addPathPatterns("/*");
--- src/main/java/com/syrisa/controller/HomeController.java
+++ src/main/java/com/syrisa/controller/HomeController.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping("/home")
     public String getHomePage(){
-        return "views/home";
+        return "home";
     }
 }
